87eff9c0716c6a946b8106a889be593ca06cd374
DannyLiehr <dannyisyelling@gmail.com>
2025-07-01 21:54:27 -0400

    Fixed bug where setting the boolean to skip the title to true not only made the disclaimer play anyway, it also would change the scene to an empty scene.

--- Scripts/disclaimer.gd
+++ Scripts/disclaimer.gd
@@ -1,5 +1,12 @@
 extends Control
 
+@export var next_scene : PackedScene = preload("res://Levels/BetaRoom.tscn")
+
+
+func _ready():
+	if GameDefaults.skip_title:
+		get_tree().change_scene_to_packed(next_scene)
+
 func _input(event: InputEvent) -> void:
 	var can_continue = get_meta("can_continue") # Call on input, since an animationplayer is modifying this.
 	if event.is_action_pressed("ui_accept") and can_continue:
